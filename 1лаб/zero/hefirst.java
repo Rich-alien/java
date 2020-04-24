1a280644d823005a93fe1ae6881eaa93227fab1d
Rich <richalien@yandex.ru>
2020-04-24 12:43:10 +0400

    lab1

---
+++ 1лаб/zero/hefirst.java
@@ -0,0 +1,6 @@
+public class hefirst{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello from first");
+    }
+}
---
+++ 1лаб/zero/hisecond.java
@@ -0,0 +1,7 @@
+
+public class hisecond {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello from second");
+    }
+}
